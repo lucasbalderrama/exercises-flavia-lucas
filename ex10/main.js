678c08dc5a850c6eaf81d4ee87f9bef1852e5081
lucasbalderrama <abreulucas3715@gmail.com>
2024-09-08 16:59:11 -0300

    Ex 2 e 10 - coreeções

--- ex10/main.js
+++ ex10/main.js
@@ -7,6 +7,7 @@ while (resposta !== numero) {
   resposta = (prompt("Adivinhe o número (entre 1 e 100):"));
 
   (resposta > numero) ? alert("Seu resposta é maior que o número definido.") : {};
-  (resposta < numero) ? alert("Seu resposta é menor que o número definido.")
-  : alert("Parabéns! Você acertou o número.");
+  (resposta < numero) ? alert("Seu resposta é menor que o número definido."): {}
 }
+
+alert("Parabéns! Você acertou o número.");
